e73f76dc5233637f1b6018ebd420258ce566f3c4
burakakcal <67555479+burakakcal@users.noreply.github.com>
2021-02-23 19:43:53 -0500

    Update style.css

--- config/globals.js
+++ config/globals.js
@@ -1,4 +1,4 @@
-// store any global variables and export so they are public
+// global variables stored for public folder prevention
 module.exports = {
     'db': 'mongodb+srv://admin:sampiyon@cluster0.jkfwe.mongodb.net/comp2106'
 }
